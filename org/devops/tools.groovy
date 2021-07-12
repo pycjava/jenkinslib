2e8ed793ac137c64c1cf946c84607076ea8a5cd7
yuxiaodong <yuxiaodong@vrgvtech.com>
2021-07-12 10:12:11 +0800

    update

--- org/devops/tools.groovy
+++ org/devops/tools.groovy
@@ -19,7 +19,6 @@ def checkOut(srcUrl, branchName) {
               userRemoteConfigs                : [[credentialsId: '0fb2d9d5-ee9e-4901-bf65-9a613a679871', url: "${srcUrl}"]]])
 }
 
-
 def sonarScan(buildType) {
     def home = buildHome(buildType)
     withSonarQubeEnv('sonarqube') {
@@ -29,7 +28,7 @@ def sonarScan(buildType) {
 }
 
 def buildHome(buildType) {
-    def buildTools = ["mvn": "M3", "ant": "ANT", "gradle": "GRADLE", "npm": "NPM"]
+    def buildTools = ['mvn': 'M3', 'ant': 'ANT', 'gradle': 'GRADLE', 'npm': 'NPM']
     println("当前选择的构建类型为 ${buildType}")
     def home = tool buildTools[buildType]
     return home
@@ -45,8 +44,8 @@ def build(buildType, buildShell) {
 def GetGav() {
     def pom = readMavenPom file: 'pom.xml'
     env.pomVersion = "${pom.version}"
-    env.pomArtifact = "${pom.properties["deploy.packname"]}"
-    if (pomArtifact == null || pomArtifact == "") {
+    env.pomArtifact = "${pom.properties['deploy.packname']}"
+    if (pomArtifact == null || pomArtifact == '') {
         env.pomArtifact = "${pom.artifactId}"
     }
     env.pomGroupId = "${pom.groupId}"
@@ -59,8 +58,8 @@ def GetGav() {
 def deploy(deployHosts) {
     GetGav()
     withCredentials([usernamePassword(credentialsId: 'nexus', passwordVariable: 'password', usernameVariable: 'username')]) {
-        def repository = "http://192.168.80.131:8081/repository/maven-snapshots/"
-        println("开始部署")
+        def repository = 'http://192.168.1.133:82/repository/maven-snapshots/'
+        println('开始部署')
         ansiblePlaybook(
                 installation: 'Ansible',
                 playbook: '/etc/ansible/deploy.yml',
@@ -74,10 +73,5 @@ def deploy(deployHosts) {
                         password:"${password}"
                 ]
         )
-        // some block
-//        ansiblePlaybook extras: 'groupId=${pomGroupId} artifactId=${pomArtifact} appVersion=${pomVersion} deployIp=${deployHosts} repository=${repository}', installation: 'Ansible', playbook: '/etc/ansible/deploy.yml'
-//        sh """
-//            ${home}/ansible-playbook /etc/ansible/deploy.yml
-//            -e "groupId=${pomGroupId} artifactId=${pomArtifact} appVersion=${pomVersion} deployIp=${deployHosts} repository=${repository}"  """
     }
 }
