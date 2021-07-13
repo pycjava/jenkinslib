dd50efcb0530f5fe32e556fd7cae310946c44d35
yuxiaodong <yuxiaodong@vrgvtech.com>
2021-07-13 08:54:55 +0800

    update

--- org/devops/tools.groovy
+++ org/devops/tools.groovy
@@ -28,18 +28,7 @@ def sonarScan(sonarServer, projectName, projectDesc, projectPath, branchName) {
         def sonarDate = sh  returnStdout: true, script: 'date  +%Y%m%d%H%M%S'
         sonarDate = sonarDate - '\n'
         sh """
-            ${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=${projectName} \
-            -Dsonar.projectName=${projectName} \
-            -Dsonar.projectVersion=${sonarDate} \
-            -Dsonar.ws.timeout=30 \
-            -Dsonar.projectDescription=${projectDesc} \
-            -Dsonar.links.homepage=http://www.baidu.com \
-            -Dsonar.sources=${projectPath} \
-            -Dsonar.sourceEncoding=UTF-8 \
-            -Dsonar.java.binaries=target/classes \
-            -Dsonar.java.test.binaries=target/test-classes \
-            -Dsonar.java.surefire.report=target/surefire-reports \
-            -Dsonar.branch.name=${branchName} -X
+            ${home}/bin/${buildType} clean verify  -Dmaven.test.skip=true sonar:sonar
         """
     }
 }
